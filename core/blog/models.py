6843f91269156f9c7dbd61420c7eb8ea2828ea1f
Mohammadi <amohammadia0086@gmail.com>
2026-02-19 22:33:27 +0330

    Set up custom User model and fix Post model
    - Set AUTH_USER_MODEL to accounts.User in settings.py
    - Fix ForeignKey references in Post model (author & category)
    - Apply initial migrations for custom User

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 class Post(models.Model):
     # this is a class to define posts for blog app
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ImageField(null=True, blank=True)
     title = models.CharField(max_length=250)
     content = models.TextField()
     status = models.BooleanField()
-    category = models.ForeignKey("category", on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey("Category", on_delete=models.SET_NULL, null=True)
 
     created_date = models.DateField(auto_now_add=True)
     updated_date = models.DateField(auto_now=True)
--- core/core/settings.py
+++ core/core/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts",
-    "blog"
+    #"blog"
 ]
 
 MIDDLEWARE = [
@@ -118,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# User manager config
+AUTH_USER_MODEL = "accounts.User"
