37581a5b4238951e8e34e74984e18818c1334836
Mohammadi <amohammadia0086@gmail.com>
2026-02-20 00:43:56 +0330

    Add Profile model to accounts app
    - Created Profile model in accounts app with fields: first_name, last_name, image, description, created_date, updated_date, and user (FK to User).
    - Registered Profile model in Django admin and updated User admin to include custom display.
    - Added Pillow to requirements.txt for image handling.
    - Generated initial migration for Profile model.

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -50,3 +50,15 @@ class User(AbstractBaseUser,PermissionsMixin):
     def __str__(self):
         # Display user email as string representation.
         return self.email
+    
+class Profile(models.Model):
+   user = models.ForeignKey(User,on_delete=models.CASCADE)
+   first_name = models.CharField(max_length=255)
+   last_name = models.CharField(max_length=255)
+   image = models.ImageField(blank=True, null=True)
+   description = models.TextField()
+   created_date = models.DateField(auto_now_add=True)
+   updated_date = models.DateField(auto_now=True)
+
+   def __str__(self):
+      return self.user.email
