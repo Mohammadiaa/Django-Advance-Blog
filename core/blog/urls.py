7dcd80b99a76eabdbd9e413d2df00a7fd2aead8d
Mohammadi <amohammadia0086@gmail.com>
2026-02-26 22:40:53 +0330

    Add TemplateView-based CBV for index page
    - Implemented Class-Based View (TemplateView) for index page
    - Added IndexView class using TemplateView
    - Moved context handling to get_context_data()
    - Passed name and posts data to template
    - Updated URLs to use CBV instead of inline TemplateView
    - Improved template to display name and posts
    
    This change demonstrates proper usage of TemplateView in Django CBVs.

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import indexView
+from . import views
 from django.views.generic import TemplateView
-urlpatterns = [
-    path("fbv-index", indexView, name="fbv-index" ),
-    path("cbv-index", TemplateView.as_view(template_name="index.html", extra_context = {"name": "ali"})),
 
+
+urlpatterns = [
+    path("fbv-index", views.indexView, name="fbv-index" ),
+   # path("cbv-index", TemplateView.as_view(template_name="index.html", extra_context = {"name": "ali"})),
+    path("cbv-index",views.Indexview.as_view(), name="cbv-index")
 ]
