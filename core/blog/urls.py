50d9c99a17278ae4066ef05a1f18b3e3d9dcd3c7
Mohammadi <amohammadia0086@gmail.com>
2026-02-27 12:12:06 +0330

    Enhance RedirectView with dynamic Post lookup
    - Added a dynamic RedirectTomaktab view that fetches a Post object by its pk before redirecting.
    - Updated URL pattern to include <int:pk> for RedirectTomaktab.
    - Improved IndexView context to include all Post objects.
    - Cleaned up imports and removed commented-out old code.

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -6,12 +6,6 @@ from django.views.generic.base import RedirectView
 app_name = "blog"
 
 urlpatterns = [
-    path("fbv-index", views.indexView, name="fbv-index" ),
-   # path("cbv-index", TemplateView.as_view(template_name="index.html", extra_context = {"name": "ali"})),
     path("cbv-index",views.Indexview.as_view(), name="cbv-index"),
-       path(
-        "go-to-index",
-        RedirectView.as_view(pattern_name= "blog:cbv-index"),
-        name="go-to-index",
-    ),
+    path("go-to-maktabkhooneh/<int:pk>/",views.RedirectTomaktab.as_view(),name="go-to-maktabkhooneh"),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from .models import Post
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
-def indexView(request):
-    # a function based view to show index page
-    name = "ali"
-    context = {"name": name}
-    return render(request, "index.html", context)
+# Function Base View Show a template
+# def indexView(request):
+#     # a function based view to show index page
+#     name = "ali"
+#     context = {"name": name}
+#     return render(request, "index.html", context)
 
 class Indexview(TemplateView):
     # a class based view to show index page
@@ -18,3 +20,15 @@ class Indexview(TemplateView):
         context['name'] = "ali"
         context["posts"] = Post.objects.all()
         return context
+    
+#FBV for redirect
+# from django.shortcuts import redirect
+# def redirectToMaktab(request):
+#     return redirect("http://maktabkhooneh.com")
+class RedirectTomaktab(RedirectView):
+    url = "http://maktabkhooneh.com"
+
+    def get_redirect_url(self, *args, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs["pk"])
+        print(post)
+        return super().get_redirect_url(*args, **kwargs)
