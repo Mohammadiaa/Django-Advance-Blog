ae48f779031136f690a0bf9894e404b24a592b59
Mohammadi <amohammadia0086@gmail.com>
2026-03-09 14:24:02 +0330

    feat: Implement post editing functionality
    Created PostEditView using Django’s generic UpdateView.
    Added a new URL pattern /post/<int:pk>/edit/ to handle post updates.
    Added an “Edit” link for each post on the post list page.

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("post/",views.PostListView.as_view(), name="post_list"),
     path("post/<int:pk>/", views.PostDetailView.as_view(), name = "post-detail"),
     path("post/create/", views.PostCreateView.as_view(), name= "post-create"),
+    path("post/<int:pk>/edit/", views.PostEditView.as_view(), name="post-edit")
    
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, RedirectView
-from django.views.generic import ListView, DetailView, FormView, CreateView
+from django.views.generic import ListView, DetailView, FormView, CreateView, UpdateView
 from .models import Post
 # from .forms import ContactForm
 from django.shortcuts import get_object_or_404
@@ -69,3 +69,8 @@ class PostCreateView(CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
         print(self.request.user)
+
+class PostEditView(UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = "/blog/post/"
