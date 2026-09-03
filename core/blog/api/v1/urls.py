86708f765eb6b8c603bcf2da952e8b05aa3bbe11
Mohammadiaa <141008520+Mohammadiaa@users.noreply.github.com>
2026-09-03 12:05:46 +0330

    feat: add basic PostSerializer and PostDetail view

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "api-v1"
 
 urlpatterns = [
-    path('post/', views.PostList, name="post-list")
+    path('post/', views.PostList, name="post-list"),
+    path('post/<int:id>/', views.PostDetail, name="post-detail"),
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,6 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import PostSerializer
+from ...models import Post
+
 
 @api_view()
 def PostList(request):
     return Response({"name":"ali"})
+
+@api_view()
+def PostDetail(request,id):
+    post = Post.objects.get(pk=id)
+    print(post.__dict__)
+    serializer = PostSerializer(post)
+    print(serializer.__dict__)
+    return Response(serializer.data)
