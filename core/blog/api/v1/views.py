5801a811174a04a89bc17adf0c7f286752f2e7f2
Mohammadiaa <141008520+Mohammadiaa@users.noreply.github.com>
2026-09-03 13:04:08 +0330

    feat: add PostList API endpoint with status filtering

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -4,14 +4,17 @@ from .serializers import PostSerializer
 from ...models import Post
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+
 @api_view()
 def PostList(request):
-    return Response({"name":"ali"})
+    posts = Post.objects.filter(status=True)
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
 
 @api_view()
 def PostDetail(request,id):
 
-    post = get_object_or_404(Post,pk=id)
+    post = get_object_or_404(Post,pk=id, status=True)
     serializer = PostSerializer(post)
     return Response(serializer.data)
     
