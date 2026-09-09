189cff4f68bd91fd235664bb7480cd4c4f86b2dc
Mohammadiaa <141008520+Mohammadiaa@users.noreply.github.com>
2026-09-09 21:35:22 +0330

    feat: implement nested CategorySerializer in PostSerializer

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -5,18 +5,23 @@ from ...models import Post, Category
 #     id = serializers.IntegerField()
 #     title = serializers.CharField(max_length=255)
 
-class PostSerializer(serializers.ModelSerializer):
-    snippet = serializers.ReadOnlyField(source='get_snippet')
-    relative_url = serializers.URLField(source="get_absolute_api_url",read_only=True)
-    absolute_url = serializers.SerializerMethodField()
-    class Meta:
-        model = Post
-        fields = ["id", "title", "content","snippet", "status","author","absolute_url","relative_url", "created_date", "published_date"]
-
-    def get_absolute_url(self,obj):
-        request = self.context.get('request')
-        return request.build_absolute_uri(obj.get_absolute_api_url())
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ["id", "name"]
+class PostSerializer(serializers.ModelSerializer):
+    snippet = serializers.ReadOnlyField(source='get_snippet')
+    relative_url = serializers.URLField(source="get_absolute_api_url",read_only=True)
+    absolute_url = serializers.SerializerMethodField()
+    # category = serializers.SlugRelatedField(many=False, slug_field="name",queryset=Category.objects.all())
+    category = CategorySerializer()
+    class Meta:
+        model = Post
+        fields = ["id", "title", "content","snippet", "status","category","author","absolute_url","relative_url", "created_date", "published_date"]
+
+    def get_absolute_url(self,obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.get_absolute_api_url())
+
+    # def to_representation(self,instance):
+    #     return super().to_representation(instance) 
