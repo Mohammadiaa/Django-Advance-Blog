f347dcffc5ed9f9fc29ad3bdf03a69ef8c05edc2
Mohammadiaa <141008520+Mohammadiaa@users.noreply.github.com>
2026-09-22 21:53:23 +0330

    feat: add ordering capability to PostModelViewSet by published_date

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -12,7 +12,7 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter,OrderingFilter
 from .paginations import DefaultPagination
 
 # @api_view(["GET","POST"])
@@ -107,9 +107,10 @@ class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
-    filter_backends = [DjangoFilterBackend,SearchFilter]
+    filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
     filterset_fields = ['category', 'author', 'status']
     search_fields = ['title', 'content']
+    ordering_fields = ['published_date']
     pagination_class = DefaultPagination
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
